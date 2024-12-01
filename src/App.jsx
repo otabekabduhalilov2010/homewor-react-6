af9fe4261fb4a9010ac06fced91213e5c3134480
otabekabduhalilov2010 <otabekabduhalilov80@gmail.com>
2024-12-01 23:32:57 +0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from './components/Header/Header'
 import Home from './pages/Home'
 import Section from './components/Section/Section'
 import Section2 from './components/Sectio2/Section2'
+import Section3 from './components/Section3/Section3'
 
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
     <Header />
     <Section />
     <Section2 />
+    <Section3 />
 
     <Routes>
       <Route path='/' element={<Home />} />
